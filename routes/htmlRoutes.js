d8fe96b9e3d4e68ee6991b2c3c96e6c1f5c745bd
theAndes <age.esquivel@gmail.com>
2019-04-22 16:58:06 -0500

    playing with EXHBS

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,12 +4,15 @@ module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
     res.render("index", {
+      tittle:"AndrewEsquivel || Dev",
       msg: "Welcome! This is the Cheerio MongoDB Demo"
     });
   });
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
-    res.render("404");
+    res.render("404",{
+tittle: "The 404 Page"
+    });
   });
 };
